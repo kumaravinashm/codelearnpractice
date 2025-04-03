b201ac856df1353c95f4dedb215d940174cf1d5a
Avinash <avinashnaa@gmail.com>
2025-04-03 20:12:13 +0530

    Initial Commit

--- src/strings/AnagramInStrings.java
+++ src/strings/AnagramInStrings.java
@@ -3,6 +3,8 @@ package strings;
 import java.util.ArrayList;
 import java.util.List;
 
+import static strings.AnagramStrings.*;
+
 public class AnagramInStrings {
 
     public static void main(String[] args) {
@@ -12,13 +14,15 @@ public class AnagramInStrings {
 
 
     public static List<Integer> findAnagrams(String s, String p) {
-        String temp;
-        List<Integer> indexes = new ArrayList<>();
-        for(int i = 0; i<s.length()-p.length()+1; i++){
+        String temp = "";
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i=0;i<s.length()-p.length()+1;i++){
             temp = s.substring(i,i+p.length());
-            if(AnagramStrings.checkAnagram(temp,temp.length(),p,p.length())) indexes.add(i);
+            if(AnagramStrings.checkAnagram(temp,temp.length(),p,p.length())){
+                list.add(i);
+            };
         }
-        return indexes;
+        return list;
     }
 
 }
--- src/strings/AnagramStrings.java
+++ src/strings/AnagramStrings.java
@@ -5,26 +5,27 @@ import java.util.Arrays;
 
 public class AnagramStrings {
     public static void main(String[] args) {
-        String s1="anagramq",s2="nagaramq";
+        String s1="^ca",s2="^ac";
         System.out.println(checkAnagram(s1,s1.length(),s2,s2.length()));
     }
 
     static boolean checkAnagram(String s1,int n1,String s2,int n2){
-        int[] a=new int[256];
-        int[] b=new int[256];
-        Arrays.fill(a,0);
-        Arrays.fill(b,0);
-        char[] c1=s1.toCharArray();
-        char[] c2=s2.toCharArray();
-        if(n1==n2){
+        if(n1==n2) {
+            int[] a = new int[256];
+            int[] b = new int[256];
+            char[] a1 = s1.toCharArray();
+            char[] b1 = s2.toCharArray();
             for(int i=0;i<n1;i++){
-                a[c1[i]]++;
-                b[c2[i]]++;
+                a[a1[i]]++;
+                b[b1[i]]++;
             }
-        }else return false;
-        for(int i=0;i<a.length;i++){
-            if(a[i]!=b[i]) return false;
-        }
-        return true;
+            for(int i=0;i<a.length;i++){
+                if(a[i]!=b[i]) {
+                    return false;
+                }
+            }
+            return true;
+        }return false;
     }
+
 }
